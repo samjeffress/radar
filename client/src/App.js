16a11fdf6fa6f8c05ae9c12e6409e1513d7819da
Samuel Jeffress <s.jeffress@nib.com.au>
2017-11-10 21:03:34 +1100

    connecting to store

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import MobxFirebaseStore from 'mobx-firebase-store';
 import { observer } from 'mobx-react';
+import {createAutoSubscriber} from 'firebase-nest';
+import firebase from 'firebase';
+
 import Radar from './components/Radar';
 import logo from './logo.svg';
 import './App.css';
 
-@observer class App extends Component {
+const subscriptionKey = 'radar';
+const collectionPath = 'radarItems';
+
+const fbApp = firebase.initializeApp({
+  apiKey: 'AIzaSyAu0xzmhWQyMfVNXFzJmVL3bPHcrVaSAkM',
+  authDomain: "localhost",
+  databaseURL: 'https://tech-radar-b0653.firebaseio.com',
+  storageBucket: 'tech-radar-b0653.firebaseio.com'
+}, "techRadar");
+ 
+const store = new MobxFirebaseStore(firebase.database(fbApp).ref());
+
+
+class App extends Component {
   state = { 
     radar: []
   };
@@ -34,6 +51,11 @@ import './App.css';
   }
 
   render() {
+    const messages = store.getData(subscriptionKey); //'radar' matches the subKey below 
+    
+    //autoSubscriber keeps track of loading and error status when using store.subscribeSubsWithPromise 
+    const { _autoSubscriberFetching: fetching, _autoSubscriberError: fetchError, error } = this.state
+
     const { radar } = this.state;
 
     return (
@@ -45,6 +67,10 @@ import './App.css';
         <p className="App-intro">
           To get started, edit <code>your Radar</code> and save to reload.
         </p>
+        <div>
+          {!messages && <p>waiting for messages</p>}
+          {messages && JSON.stringify(messages, null, 2)}
+        </div>
         
         {radar.length ? <Radar items={radar} updateItem={this.updateItem} /> : <p>probably should add some data</p>}
       </div>
@@ -52,4 +78,16 @@ import './App.css';
   }
 }
 
-export default App;
+//Subscribe to and observe firebase data 
+export default createAutoSubscriber({
+  getSubs: (props, state) => [{
+    subKey: subscriptionKey, //any unique string describing this subscription; must match getData call 
+    asList: true, //or asValue: true. asList will internally subscribe via firebase child_added/removed/changed; asValue via onValue. 
+    path: collectionPath, //firebase location, 
+    
+    //Optional - get data callbacks after store data is already updated: 
+    onData: (type, snapshot) => console.log('got data: ', type, 'myMsgs', snapshot.val())
+  }], 
+  
+  subscribeSubs: (subs, props, state) => store.subscribeSubsWithPromise(subs)
+})(observer(App));
